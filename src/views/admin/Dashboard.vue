850ead7fb2379935ac8630ac162d6983c5159262
Odirlei Borgert <odirleiborgert@gmail.com>
2018-08-21 22:06:49 -0300

    Add link github dashboard

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -8,6 +8,7 @@
                         <span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum.</span>
                         <v-divider class="my-3"></v-divider>
                         <router-link :to="{ name: 'admin.page.one'}"><v-btn class="mx-0" color="primary" large>Go to Page One</v-btn></router-link>
+                        <a href="https://github.com/borgert-inc/vue-pwa-startkit"><v-btn color="primary" large><v-icon class="mr-2">launch</v-icon> Github</v-btn></a>
                     </v-flex>
                 </v-layout>
             </v-container>
